456c7b2e365896a50098c83b532293507c08b00d
prosecco75 <pygmalion1999@gmail.com>
2015-11-04 23:16:37 +0000

    postnotworking

--- swing/db/schema.rb
+++ swing/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151103180224) do
+ActiveRecord::Schema.define(version: 20151104225826) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20151103180224) do
     t.text     "review"
     t.integer  "rating"
     t.string   "title"
+    t.string   "post_image"
   end
 
   create_table "users", force: :cascade do |t|
